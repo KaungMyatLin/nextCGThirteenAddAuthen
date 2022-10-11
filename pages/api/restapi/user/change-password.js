0db3f6150b8ee5cc3580e755c6869ff1867fa6e0
KML <kaungmyatlin.worklife@gmail.com>
2022-10-12 01:16:37 +0630

    a chngPw logic in restapi.

--- pages/api/restapi/user/change-password.js
+++ pages/api/restapi/user/change-password.js
@@ -1,4 +1,6 @@
 import { unstable_getServerSession } from 'next-auth/next'
+import { pwCanbeSameHarsh, c_hashPw } from '../../../lib_common/auth';
+import connectToMongoClient from '../../../lib_common/db';
 
 const chgPwHdl = async (req, res) => {
     if (req.method !== "PATCH") {
@@ -9,9 +11,35 @@ const chgPwHdl = async (req, res) => {
     console.log("chgPwHdl:11 sessionObj=")
     console.log(sessionObj)
     if (!sessionObj) {
-        res.status(401).json({message: `Not authenticated by sensing request to this api's header cookie's authorization code`})
+        res.status(401).json({message: `401 Not authenticated by sensing request to this api's header cookie's authorization code`})
         return;
     }
+    const r_em_SessionObj = sessionObj?.user?.email;
+    const oldPw = req.body.oldPassword
+    const newPw = req.body.newPassword
+
+    const client = await connectToMongoClient();
+    const users = client.db().collection('users')
+    const user = await users.findOne({email: r_em_SessionObj})
+    if (!users) {
+        res.status(404).json({message: `user not found.!`})
+        client.close();
+        return;
+    }
+
+    const currentPw = user.password
+    const isSamePw = await pwCanbeSameHarsh(oldPw, currentPw)
+    if (!isSamePw) {
+        // res.status(403).json({message: `403 Not authorized, you don't have permission to do this operation.`})
+        res.status(422).json({message: `422 Not valid user input. Could not perform changing password.`})
+        client.close();
+        throw new Error('Invalid password! Try again!');
+    }
+
+    const hashDigest = await c_hashPw(newPw);
+    const result = await users.updateOne({email: r_em_SessionObj}, {$set: {password: newPw}})
+    client.close();
+    res.status(200).json({message: `password has been successfully changed!`})
 }
 
 export default chgPwHdl
