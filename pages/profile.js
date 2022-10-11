3ce7a08b9d29ae489e695ae683150ff67de3ed6b
KML <kaungmyatlin.worklife@gmail.com>
2022-10-11 16:47:56 +0630

    adding serversidepage guard.

--- pages/profile.js
+++ pages/profile.js
@@ -5,19 +5,25 @@ import { authOptions } from "./api/auth/[...nextauth]"
 function ProfilePage(props) {
   return <UserProfile />;
 }
+// adding serversidepage guard.
 export async function getServerSideProps(context) {
-  const sessionObj = await unstable_getServerSession(context.req, context.res, authOptions)
+  const sessionObj = await unstable_getServerSession(context.req, context.res, authOptions) // unstable_getServerSession is experimental, no alternative in v4.
+  console.log("ProfilePage:11 sessionObj=")
   console.log(sessionObj)
   if (!sessionObj) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,       // if only times session is not loggedin.
-      }
-    }
+    // return {
+    //   redirect: {
+    //     destination: '/auth',
+    //     permanent: false,       // if only times session is not loggedin.
+    //   }
+    // }
   }
+  const jsonStr = JSON.stringify(sessionObj)
+  const converted_sessObj = JSON.parse(jsonStr)
+  console.log("ProfilePage:22 converted_sessObj=")
+  console.log(converted_sessObj)
   return {
-    props: { session: sessionObj}
+    props: { session: converted_sessObj}
   }
 }
 
